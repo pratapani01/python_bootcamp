ac166fe822bef40a66b4d5a97e55979094cc475e
pratapani01 <pratapani01@gmail.com>
2026-09-22 10:33:44 +0530

    chapter 2

---
+++ chapter2/input.py
@@ -0,0 +1,16 @@
+a=input("Enter 1:")
+b=input("Enter 2:")
+
+print(a)
+print(b)
+sum=a+b
+print(sum)
+
+
+a=int(input("Enter 1:"))
+b=int(input("Enter 2:"))
+
+print(a)
+print(b)
+sum=a+b
+print(sum)
---
+++ chapter2/operators.py
@@ -0,0 +1,15 @@
+a= True or False
+
+print(a)
+
+b= False and True
+
+print(b)
+
+c= not False
+
+print(c)
+
+d= not True
+
+print(d)
